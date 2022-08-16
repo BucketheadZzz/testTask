0782c9987378f64c19127a12c5d7d6bf90bcfd4d
Dzmitry Khilimon <d.khilimon@gmail.com>
2022-08-16 21:01:35 +0300

    some changes

--- testTask/testTaskApp/testTaskAppB/wwwroot/js/site.js
+++ testTask/testTaskApp/testTaskAppB/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿$(function() {
+﻿
+// Задачу можно было бы просто через CSS сделать
+$(function () {
     $(".hover-grid tbody tr").hover(function (e) {
           $(this).toggleClass("hover");
     });
